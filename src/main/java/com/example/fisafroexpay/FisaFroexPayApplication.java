cfd02a44994fa9811b44011e6e3e907daed9b143
SeokCheol <lsc991123@gmail.com>
2024-08-09 16:43:32 +0900

    init

---
+++ src/main/java/com/example/fisafroexpay/FisaFroexPayApplication.java
@@ -0,0 +1,13 @@
+package com.example.fisafroexpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisaFroexPayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FisaFroexPayApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/fisafroexpay/FisaFroexPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fisafroexpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisaFroexPayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
