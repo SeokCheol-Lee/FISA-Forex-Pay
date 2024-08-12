d03af218592590ec7fb791e1ab8bfd5944e3888f
lisiant <wkdxod321@gmail.com>
2024-08-12 19:03:26 +0900

    Feat: TotalAccountRepository 생성

---
+++ src/main/java/com/example/fisafroexpay/repository/TotalAccountRepository.java
@@ -0,0 +1,7 @@
+package com.example.fisafroexpay.repository;
+
+import com.example.fisafroexpay.entity.TotalAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TotalAccountRepository extends JpaRepository<TotalAccount, Long> {
+}
