d3b5501e61fb28adcc9759ad06070dda7cdebe7c
hyleei <qkrgustjtj@naver.com>
2024-08-13 18:00:54 +0900

    Refactor: @Controller 어노테이션, URL, redirection 변경

--- src/main/java/com/example/fisafroexpay/controller/UserAccessController.java
+++ src/main/java/com/example/fisafroexpay/controller/UserAccessController.java
@@ -3,22 +3,20 @@ package com.example.fisafroexpay.controller;
 import com.example.fisafroexpay.dto.RegisterForm;
 import com.example.fisafroexpay.service.UserAccessService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/login")
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class UserAccessController {
 
   private final UserAccessService userAccessService;
 
-  @PostMapping("/join")
-  public String join(@RequestBody RegisterForm registerForm) {
+  @PostMapping("/signup")
+  public String join(@ModelAttribute RegisterForm registerForm) {
     userAccessService.registerUser(registerForm);
-    return "redirect:/login";
+    return "redirect:/login.html";
   }
 
 }
