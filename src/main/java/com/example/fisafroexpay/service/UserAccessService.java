742cc0b110bab3aa20398875868e7ea79ed50f18
lisiant <wkdxod321@gmail.com>
2024-08-13 18:12:27 +0900

    Refactor: User field 변수명 변경으로 인한 Builder 패턴 수정

--- src/main/java/com/example/fisafroexpay/service/UserAccessService.java
+++ src/main/java/com/example/fisafroexpay/service/UserAccessService.java
@@ -3,7 +3,9 @@ package com.example.fisafroexpay.service;
 import com.example.fisafroexpay.dto.RegisterForm;
 import com.example.fisafroexpay.entity.User;
 import com.example.fisafroexpay.repository.UserRepository;
+
 import java.math.BigDecimal;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,17 +18,18 @@ public class UserAccessService {
     private final PasswordEncoder passwordEncoder;
 
     public void registerUser(RegisterForm form) {
-        if(userRepository.existsByEmail(form.getEmail())) {
+        if (userRepository.existsByEmail(form.getEmail())) {
             throw new IllegalArgumentException("Email already in use");
         }
         User user = User.builder()
-            .username(form.getUsername())
-            .email(form.getEmail())
-            .password(passwordEncoder.encode(form.getPassword()))
-            .total_assets(0L)
-            .discount_rate(BigDecimal.valueOf(0))
-            .gender(form.getGender())
-            .age(form.getAge()).build();
+                .username(form.getUsername())
+                .email(form.getEmail())
+                .password(passwordEncoder.encode(form.getPassword()))
+                .totalAssets(0L)
+                .discountRate(BigDecimal.valueOf(0))
+                .gender(form.getGender())
+                .age(form.getAge()).build();
+
         userRepository.save(user);
     }
 
